ce83a0ac322166b52c495244051ad0bed28f5359
Agung Julian Perkasa <agungjulianperkasa@gmail.com>
2022-10-20 14:45:21 +0700

    test

---
+++ send-image.php
@@ -0,0 +1,26 @@
+<?php
+$curl = curl_init();
+$token = "";
+$data = [
+'phone' => '6282119661621-1623510812',
+'image' => 'http://divrsd.online/images/Logo_PLN.png',
+'caption' => 'tes',
+];
+curl_setopt($curl, CURLOPT_HTTPHEADER,
+    array(
+        "Authorization: $token",
+    )
+);
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($curl, CURLOPT_URL,  "https://jogja.wablas.com/api/send-image");
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+
+$result = curl_exec($curl);
+curl_close($curl);
+echo "<pre>";
+print_r($result);
+
+?>
